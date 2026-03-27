70d8b717eacc8d8d8ea4600493a82fc5027dd846
Skirmitch <your@email.com>
2026-03-27 13:30:27 -0300

    correcting DS from Home-manager

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -1,7 +1,15 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     jq
-    discord
+    (pkgs.symlinkJoin {
+    name = "discord";
+    paths = [ pkgs.discord ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/discord \
+        --unset NIXOS_OZONE_WL
+    '';
+  })
     vivaldi
     git
     gedit
