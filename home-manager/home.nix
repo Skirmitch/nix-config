683830d455af29e96942c5f52d38694344f18206
Skirmitch <your@email.com>
2026-03-29 08:40:45 -0300

    fixed update alias

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -16,7 +16,8 @@
     enable = true;
     shellAliases = {
       rebuild = "sudo nixos-rebuild switch --flake ~/nix-config#diana";
-      update = "sudo nixos-rebuild switch --flake ~/nix-config#diana --upgrade";
+      diff = "nix store diff-closures /run/current-system $(sudo nix build --no-link --print-out-paths ~/nix-config#nixosConfigurations.diana.config.system.build.toplevel 2>/dev/null)";
+      update = "sudo nix flake update ~/nix-config && sudo nixos-rebuild switch --flake ~/nix-config#diana";
       nordconnect = "sudo wgnord connect";
       norddisconnect = "sudo wgnord disconnect";
     };
