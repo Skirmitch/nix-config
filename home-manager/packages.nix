3cf90deeee37f5cb004b2c8e6a94c24bc5e2b6e0
Skirmitch <your@email.com>
2026-03-30 09:04:13 -0300

    Fixed DS launcher cuz of global wayland thingy

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -20,4 +20,13 @@
     gnomeExtensions.kimpanel
     nvitop
   ];
+
+  xdg.desktopEntries.discord = {
+    name = "Discord";
+    exec = "discord";
+    icon = "discord";
+    genericName = "All-in-one cross-platform voice and text chat for gamers";
+    categories = [ "Network" "InstantMessaging" ];
+    mimeType = [ "x-scheme-handler/discord" ];
+  };
 }
