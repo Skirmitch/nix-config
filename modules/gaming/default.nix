19ea3c75177034a39631e7344bbb0aeda0754af4
Skirmitch <your@email.com>
2026-04-18 10:04:39 -0400

    Added Steam controller fix

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [ ./aagl.nix ];
 
   # --- GAMING ---
@@ -8,4 +8,25 @@
   options bluetooth disable_ertm=1
   options xpadneo disable_deadzones=0
   '';
+
+
+  # Fix for systemd 258 breaking xpadneo input (NixOS unstable bug)
+  services.udev.packages = [
+    (pkgs.writeTextFile {
+      name = "60-xpadneo";
+      text = ''
+        ACTION=="bind", SUBSYSTEM=="hid", DRIVER!="xpadneo", KERNEL=="0005:045E:*", KERNEL=="*:02FD.*|*:02E0.*|*:0B05.*|*:0B13.*|*:0B20.*|*:0B22.*", ATTR{driver/unbind}="%k", ATTR{[drivers/hid:xpadneo]bind}="%k"
+        ACTION!="remove", DRIVERS=="xpadneo", SUBSYSTEM=="input", TAG+="uaccess"
+      '';
+      destination = "/etc/udev/rules.d/60-xpadneo.rules";
+    })
+    (pkgs.writeTextFile {
+      name = "70-xpadneo-disable-hidraw";
+      text = ''
+        ACTION!="remove", DRIVERS=="xpadneo", SUBSYSTEM=="hidraw", MODE:="0000", TAG-="uaccess"
+      '';
+      destination = "/etc/udev/rules.d/70-xpadneo-disable-hidraw.rules";
+    })
+  ];
+}
 }
