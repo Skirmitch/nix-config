d5bdd4c1cd11498b3a86aeaaae6161ee6e2b7218
Skirmitch <your@email.com>
2026-04-19 18:17:48 -0400

    Fixed dif alias

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -24,7 +24,7 @@
       }
     '';
     shellAliases = {
-      dif = "nix store diff-closures /run/current-system $(sudo nix build --no-link --print-out-paths ~/nix-config#nixosConfigurations.diana.config.system.build.toplevel 2>/dev/null)";
+      dif = "cd /home/skirmitch/nix-config && nix flake update && nixos-rebuild build --flake ~/nix-config#diana && nvd diff /run/current-system ./result";
       update = "cd /home/skirmitch/nix-config && sudo nix flake update /home/skirmitch/nix-config && rebuild \"Updated previous system\"";
       nordconnect = "sudo wgnord connect";
       norddisconnect = "sudo wgnord disconnect";
