2c91c43b91b5219c5660830e7432c6b9056687e8
Skirmitch <your@email.com>
2026-04-19 18:14:26 -0400

    Updated previous system

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -3,5 +3,6 @@
   boot.loader.systemd-boot = { enable = true; configurationLimit = 10; };
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelModules = [ "asus_ec_sensors" ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.initrd.systemd.enable = false;
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
 }
