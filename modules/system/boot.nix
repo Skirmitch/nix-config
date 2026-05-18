283e4c8365fc3e7a2ca0534779eee3878bbdc098
Skirmitch <your@email.com>
2026-05-17 20:38:53 -0400

    Updated previous system

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -4,5 +4,15 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelModules = [ "asus_ec_sensors" ];
   boot.initrd.systemd.enable = false;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # Pinned to 7.0.5: 7.0.8 regressed MT7921U BT (WMT cmd failure on init).
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_latest.override {
+    argsOverride = rec {
+      version = "7.0.5";
+      modDirVersion = version;
+      src = pkgs.fetchurl {
+        url = "mirror://kernel/linux/kernel/v7.x/linux-${version}.tar.xz";
+        hash = "sha256-ll+wocFnU5n8YMYGOyJ8BSMEG1+aZitmRi8SEsQ4rDw=";
+      };
+    };
+  });
 }
